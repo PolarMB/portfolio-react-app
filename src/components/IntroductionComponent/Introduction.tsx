65ee9cac32786e82844c27dd8127af880b021114
Omar Vivenes <omarrgvm.97@gmail.com>
2022-11-18 00:40:59 -0300

    added Image element to Introduction component

--- src/components/IntroductionComponent/Introduction.tsx
+++ src/components/IntroductionComponent/Introduction.tsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Image } from 'react-bootstrap';
+import CvPicture from '../../images/cvPicture.jpeg';
 
 const Introduction: React.FC = () => {
 
   return (
     <>
-    
+      <Image src={CvPicture} alt='Image not found'
+        width={330}
+        height={350}
+        className='ml-5 mr-n5'
+        rounded></Image>
     </>
   );
 }
--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import Introduction from '../components/IntroductionComponent/Introduction';
 
 
 function TestComponent(props: any) {
   return (
     <>
       <h1>Hello, {props.name}</h1>
+      <Introduction></Introduction>
     </>
   );
 }
